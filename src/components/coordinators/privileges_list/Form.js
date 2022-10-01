7b5ab4edfd494bf5624fba5e3b8f2e97a24965f6
NirajSharma156 <nirajssharma1@gmail.com>
2022-10-01 22:26:32 +0530

    changes

--- src/components/coordinators/privileges_list/Form.js
+++ src/components/coordinators/privileges_list/Form.js
@@ -23,12 +23,7 @@ import Button from "@mui/material/Button";
 import axios from "axios";
 import BackdropUI from "./utilities3/Backdrop";
 import DialogUI from "./utilities4/Dialog";
-import Fab from "@mui/material/Fab";
-import ListAltIcon from "@mui/icons-material/ListAlt";
-import Form2UI from "./utilities4/Create";
-import SearcUI2 from "./utilities4/Search2";
-import RefreshIcon from "@mui/icons-material/Refresh";
-import FormUI from "./utilities4/Form";
+import UpdateUI from "./utilities/Update";
 
 export default function AnnounceACompany() {
   const m1 = useMediaQuery("(min-width:600px)");
@@ -49,55 +44,11 @@ export default function AnnounceACompany() {
   const [loading, setLoading] = React.useState(false);
   const [dialog, setDialog] = React.useState(false);
   const [link, setLink] = React.useState("");
-  const [form2, setForm2] = React.useState(false);
-  const [forms, setForms] = React.useState(null);
-  const [forms2, setForms2] = React.useState(null);
-
-  const [activeForms, setActiveForms] = React.useState(null);
-  const [inactiveForms, setInActiveForms] = React.useState(null);
-
-  async function fetchForms() {
-    if (!companies) return;
-
-    const { data, error } = await supabase.from("forms").select("*");
-
-    if (data) {
-      const ac = [];
-      const inac = [];
-      const al = [];
-      let hash = {};
-
-      for (let j = 0; j < companies.length; j++) {
-        hash[companies[j].id] = companies[j];
-      }
-      console.log("Hash");
-      console.log(hash);
-
-      if (Object.keys(hash).length == 0) return;
-      for (let i = 0; i < data.length; i++) {
-        if (
-          data[i].start_time <= Date.now() &&
-          Date.now() <= data[i].end_time
-        ) {
-          ac.push({ data: data[i], company: hash[data[i].company_id] });
-        } else {
-          inac.push({ data: data[i], company: hash[data[i].company_id] });
-        }
-
-        al.push({ data: data[i], company: hash[data[i].company_id] });
-      }
-
-      setActiveForms(ac);
-      setInActiveForms(inac);
-      setForms(al);
-      setForms2(al);
-      console.log("Forms");
-      console.log(al);
-      // console.log(ac);
-      // console.log(inac);
-    }
-  }
-
+  const [model, setModel] = React.useState(false);
+  
+  const toggleModel = () => {
+    setModel(!model);
+  };
   async function fetchTheProfile() {
     const data = await supabase.auth.user();
 
@@ -133,51 +84,145 @@ export default function AnnounceACompany() {
     }
   }
 
+  async function openFormsHandler() {
+    let comp = null;
+    console.log(students);
+    const emails = [];
+    setLoading(true);
+
+    for (let i = 0; i < students.length; i++) {
+      emails.push(students[i].email);
+    }
+
+    if (end - start <= 0) {
+      alert("Invalid Start and end time");
+      return;
+    }
+
+    if (!control) {
+      alert("Select the checkbox");
+      return;
+    }
+
+    for (let i = 0; i < companies.length; i++) {
+      if (company == companies[i].name) {
+        comp = companies[i];
+        break;
+      }
+    }
+    console.log(company);
+    console.log(comp);
+    let uniid = "";
+
+    for (let j = 0; j < company.length; j++) {
+      if (company[j] == " ") continue;
+      uniid = uniid + company[j];
+    }
+
+    let t = Date.now();
+    setLink(
+      window.location.href.substr(0, window.location.href.length - 11) +
+        "company/" +
+        uniid +
+        t
+    );
+
+    const uploadData = {
+      company_id: comp.id,
+      start_time: start,
+      end_time: end,
+      start: sstart,
+      end: send,
+      route_id: uniid + t,
+      time_created: t,
+      url:
+        window.location.href.substr(0, window.location.href.length - 11) +
+        "company/" +
+        uniid +
+        t,
+    };
+
+    console.log(uploadData);
+
+    await axios
+      .post(process.env.REACT_APP_API_ENDPOINT, {
+        htm: ` <div>
+        <i>Apply for <b>${company}</b> Now !</i>
+        <p></p>
+        <a href="${
+          window.location.href.substr(0, window.location.href.length - 11) +
+          "company/" +
+          uniid +
+          t
+        }" style="margin-top:10px;width:100%;">${
+          window.location.href.substr(0, window.location.href.length - 11) +
+          "company/" +
+          uniid +
+          t
+        }</a>
+        <h3 style="text-align:right;marin-top:10px;"><b>- Placements NIE</b></h3>
+      </div>`,
+        text: `Apply for ${company} Now`,
+        subject: `${company} - Campus Placements`,
+        to: emails,
+        attachments: [],
+      })
+      .then((u) => {
+        console.log("Success");
+        console.log(u);
+      })
+      .catch((err) => {
+        console.log("Error");
+        console.log(err);
+      });
+
+    const { data, error } = await supabase.from("forms").insert([uploadData]);
+
+    if (data) {
+      setLoading(false);
+      setDialog(true);
+      return;
+    }
+
+    if (error) {
+      setLoading(false);
+      alert(error.message);
+    }
+  }
+
+  async function searchCompanyResults(str) {
+    console.log("Seach company result");
+    console.log(str);
+    // let com = null;
+
+    for (let i = 0; i < companies.length; i++) {
+      if (str == companies[i].name) {
+        
+       setCurrcompany(companies[i]);
+        break;
+      }
+    }
+    
+    
+    setCompany(str);
+  }
+
   React.useEffect(() => {
-    if (companies.length === 0 || !forms) {
+    if (companies.length === 0) {
       fetchTheCompanies();
       fetchTheStudents();
-      fetchForms();
     }
 
     setInterval(() => {
       fetchTheProfile();
-      // if (!forms) {
-      //   fetchTheCompanies();
-      //   fetchTheStudents();
-      //   fetchForms();
-      // }
     }, 1000);
   });
 
-  async function searchFormResults(fname) {
-    const temp = [];
-
-    if (!fname) {
-      setForms2(forms);
-      return;
-    }
-    for (let i = 0; i < forms.length; i++) {
-      if (fname == forms[i].company.name) {
-        temp.push(forms[i]);
-      }
-    }
-
-    setForms2(temp);
-  }
-
   return (
     <div>
-      {data && forms ? (
+      {data ? (
         <div>
           <CssBaseline />
-          {form2 ? (
-            <Form2UI
-              registerModalHandler={() => {
-                setForm2(!form2);
-              }}
-            />
-          ) : null}
           {loading ? <BackdropUI /> : null}
           {dialog ? (
             <DialogUI
@@ -214,107 +259,201 @@ export default function AnnounceACompany() {
                 >
                   Create Forms
                 </Typography>
-                <div
+                <Typography
+                  variant="h5"
+                  align="center"
+                  color="text.secondary"
+                  paragraph
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    marginBottom: "-20px",
+                    fontSize: m1 ? "17px" : "16px",
+                    marginBottom: "-17px",
                   }}
                 >
-                  {forms ? (
-                    <Fab
-                      variant="extended"
-                      style={{
-                        backgroundColor: "#541554",
-                        color: "white",
-                        paddingRight: "20px",
-                      }}
-                      onClick={() => {
-                        setForm2(true);
-                      }}
-                    >
-                      <ListAltIcon sx={{ mr: 1 }} />
-                      Create Form
-                    </Fab>
-                  ) : null}
-                </div>
+                  <i>
+                    “You can create forms by selecting a compnay, its opening
+                    time and its closing time. You can share the link with
+                    students. Later you can download the data.”
+                  </i>
+                </Typography>
               </Container>
             </Box>
-            <Paper
-              style={{
-                width: "95%",
-                borderRadius: "20px",
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "20px",
-              }}
-            >
-              <div>
-                <h1
-                  style={{
-                    textAlign: "center",
-                    fontFamily: "inherit",
-                    fontWeight: 500,
-                  }}
-                >
-                  Forms List
-                </h1>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    // paddingLeft: "10%",
-                    // paddingRight: "10%",
-                    width: "100%",
-                  }}
-                >
-                  <div style={{ minWidth: "70%" }}>
-                    {forms && (
-                      <SearcUI2
-                        forms={forms}
-                        searchFormResults={searchFormResults}
-                      />
-                    )}
-                  </div>
-                  <div style={{ width: "20%", marginLeft: "10%" }}>
-                    <Fab
-                      color="primary"
-                      aria-label="add"
-                      style={{
-                        width: "50px",
-                        height: "50px",
-                        backgroundColor: "black",
-                      }}
-                      onClick={() => {
-                        window.location.reload();
-                      }}
-                    >
-                      <RefreshIcon />
-                    </Fab>
-                  </div>
-                </div>
-                <br />
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    width: "100%",
-                  }}
-                >
-                  <div style={{ minWidth: "100%", maxWidth: "111%" }}>
-                    {forms2 &&
-                      forms2.map((item) => {
-                        return (
-                          <div key={item}>
-                            <FormUI data={item} />{" "}
-                          </div>
-                        );
-                      })}
-                  </div>
-                </div>
-                <br />
-                <br />
+            <Paper style={{ marginTop: "30px", borderRadius: "40px" }}>
+              <div style={{ height: "20px" }}></div>
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <img
+                  src={formimage}
+                  alt="formimage"
+                  style={{ width: "50px", height: "auto" }}
+                />
               </div>
+              <br />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  minWidth: "100%",
+                }}
+              >
+                <div style={{ width: "85%" }}>
+                  {companies && (
+                    <SearchUI
+                      companies={companies}
+                      searchCompanyResults={searchCompanyResults}
+                    />
+                  )}
+                </div>
+              </div>
+              {company ? (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "20px",
+                  }}
+                >
+                  {/* <Accordion style={{ width: "85%" }} elevation={1}>
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                    >
+                      <Typography>View more details</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography>
+                        Details about compnay should be displayed
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion> */}
+               <Button
+                variant="contained"
+                style={{
+                  backgroundColor: "#541554",
+                  color: "white",
+                  borderRadius: "15px",
+                }}
+                onClick={toggleModel}
+              >
+                View More Details
+              </Button>
+                </div>
+              ) : null}
+
+{model && currcompany ? (
+       
+        <UpdateUI
+        name={currcompany.name}
+        ctc={currcompany.ctc}
+        type={currcompany.type}
+        el={currcompany.eligible_branches}
+        t={currcompany.tentative_interview_dates}
+        mt={currcompany.min_in_ten}
+        mtw={currcompany.min_in_twelve}
+        max={currcompany.max_year_education_gap}
+        ba={currcompany.active_backlogs_allowed}
+        hba={currcompany.history_backlogs_allowed}
+        cgpa={currcompany.min_cgpa}
+        gender={currcompany.gender}
+        desc={currcompany.description}
+        id={currcompany.id}
+        jds={currcompany.jds}
+          toggleModel={toggleModel}
+        />
+       
+    
+      ) : null}
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "50px",
+                }}
+              >
+                <DateUI
+                  text="Open at"
+                  timeHandler={setStart}
+                  timeHandler2={setSStart}
+                />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "30px",
+                }}
+              >
+                <DateUI
+                  text="Close at"
+                  timeHandler={setEnd}
+                  timeHandler2={setSEnd}
+                />
+              </div>
+              {company && start && end ? (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "100%",
+                    marginTop: "20px",
+                  }}
+                >
+                  <FormGroup style={{ width: "80%" }}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          style={{ color: "#541554" }}
+                          checked={control}
+                          onChange={(e) => {
+                            setControl(e.target.checked);
+                          }}
+                        />
+                      }
+                      label={
+                        <div>
+                          <p style={{ fontSize: "13px" }}>
+                            Company - <b>{company}</b>
+                          </p>
+                          <p style={{ fontSize: "13px", marginTop: "-10px" }}>
+                            Opens at - <b>{sstart.substr(0, 25)}</b>
+                          </p>
+                          <p style={{ fontSize: "13px", marginTop: "-10px" }}>
+                            Closes at - <b>{send.substr(0, 25)}</b>
+                          </p>
+                        </div>
+                      }
+                    />
+                  </FormGroup>
+                </div>
+              ) : null}
+              {company && start && end ? (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "100%",
+                    marginTop: company && start && end ? "5px" : "30px",
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    style={{
+                      backgroundColor: "black",
+                      width: "85%",
+                      borderRadius: "10px",
+                    }}
+                    onClick={openFormsHandler}
+                  >
+                    Submit
+                  </Button>
+                </div>
+              ) : null}
+              <div style={{ height: "60px" }}></div>
             </Paper>
           </main>
         </div>
