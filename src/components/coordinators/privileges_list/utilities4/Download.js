4a7ba909ac02cf3e9bc001eeee8a09d0221e52d9
Jagannath R Kulakarni <jagannathrkulakarni.171845@gmail.com>
2022-10-03 00:58:55 +0530

    report download half done

--- src/components/coordinators/privileges_list/utilities4/Download.js
+++ src/components/coordinators/privileges_list/utilities4/Download.js
@@ -9,6 +9,7 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import Checkbox from "@mui/material/Checkbox";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import { CSVLink } from "react-csv";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -53,9 +54,89 @@ export default function Register(props) {
     props.toggleModel();
   };
 
-  const getReport = () => {
-    alert("Here");
+  const data = [
+    {
+      firstName: "Warren",
+      lastName: "Morrow",
+      email: "sokyt@mailinator.com",
+      age: "36",
+    },
+    {
+      firstName: "Gwendolyn",
+      lastName: "Galloway",
+      email: "weciz@mailinator.com",
+      age: "76",
+    },
+    {
+      firstName: "Astra",
+      lastName: "Wyatt",
+      email: "quvyn@mailinator.com",
+      age: "57",
+    },
+    {
+      firstName: "Jasmine",
+      lastName: "Wong",
+      email: "toxazoc@mailinator.com",
+      age: "42",
+    },
+    {
+      firstName: "Brooke",
+      lastName: "Mcconnell",
+      email: "vyry@mailinator.com",
+      age: "56",
+    },
+    {
+      firstName: "Christen",
+      lastName: "Haney",
+      email: "pagevolal@mailinator.com",
+      age: "23",
+    },
+    {
+      firstName: "Tate",
+      lastName: "Vega",
+      email: "dycubo@mailinator.com",
+      age: "87",
+    },
+    {
+      firstName: "Amber",
+      lastName: "Brady",
+      email: "vyconixy@mailinator.com",
+      age: "78",
+    },
+    {
+      firstName: "Philip",
+      lastName: "Whitfield",
+      email: "velyfi@mailinator.com",
+      age: "22",
+    },
+    {
+      firstName: "Kitra",
+      lastName: "Hammond",
+      email: "fiwiloqu@mailinator.com",
+      age: "35",
+    },
+    {
+      firstName: "Charity",
+      lastName: "Mathews",
+      email: "fubigonero@mailinator.com",
+      age: "63",
+    },
+  ];
+
+  const headers = [
+    { label: "First Name", key: "firstName" },
+    { label: "Last Name", key: "lastName" },
+    { label: "Email", key: "email" },
+    { label: "Age", key: "age" },
+  ];
+
+  const csvReport = {
+    data: data,
+    headers: headers,
+    filename: "Clue_Mediator_Report.csv",
   };
+
+  const getReport = () => {};
 
   return (
     <div>
@@ -833,20 +914,22 @@ export default function Register(props) {
                     width: "100%",
                   }}
                 >
-                  <Button
-                    variant="contained"
-                    style={{
-                      backgroundColor: "black",
-                      width: "80%",
-                      height: "50px",
-                      borderRadius: "16px",
-                      marginTop: "20px",
-                      marginBottom: "25px",
-                    }}
-                    onClick={getReport}
-                  >
-                    Download
-                  </Button>
+                  <CSVLink {...csvReport} style={{ width: "80%" }}>
+                    <Button
+                      variant="contained"
+                      style={{
+                        backgroundColor: "black",
+                        width: "100%",
+                        height: "50px",
+                        borderRadius: "16px",
+                        marginTop: "20px",
+                        marginBottom: "25px",
+                      }}
+                      onClick={getReport}
+                    >
+                      Download
+                    </Button>
+                  </CSVLink>
                 </div>
               </Paper>
               <br />
