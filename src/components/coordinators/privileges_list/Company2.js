2e0a46bbb397659d90789762c255a9f78f54b8a0
Jagannath_R_Kulakarni <64036703+JAGANNATH-R-KULAKARNI@users.noreply.github.com>
2023-01-05 12:59:11 +0530

    changes2

--- src/components/coordinators/privileges_list/Company2.js
+++ src/components/coordinators/privileges_list/Company2.js
@@ -34,11 +34,10 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Company3UI from "./Company3";
 
 export default function AnnounceACompany() {
   const m1 = useMediaQuery("(min-width:600px)");
-  const [width, setWidth] = React.useState(0);
-  const caro = React.useRef();
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -73,7 +72,6 @@ export default function AnnounceACompany() {
     console.log("After filter");
     console.log(temp);
     setFCompanies(temp);
-    setWidth(caro.current.scrollWidth - caro.current.offsetWidth);
   }
 
   async function searchCompanyResults(str) {
@@ -124,16 +122,11 @@ export default function AnnounceACompany() {
         if (data[i]["id"] !== 0) {
           temp.push(data[i]);
           times.push(await timeSince(new Date(parseInt(data[i].time_posted))));
-          //   console.log("HEREEE");
-          //   console.log(data[i].time_posted);
-          //   console.log(new Date(parseInt(data[i].time_posted)));
         }
       }
       setFCompanies(temp);
       setTimes(times);
-      setWidth(caro.current.scrollWidth - caro.current.offsetWidth);
       setCompanies(temp);
-      // filterCompanies3(data);
     }
   }
 
@@ -155,194 +148,9 @@ export default function AnnounceACompany() {
 
   return (
     <div style={{ width: "100%" }}>
-      {m1 ? (
-        <h3
-          style={{
-            marginLeft: "5%",
-            marginTop: "10px",
-            marginBottom: "0px",
-            fontWeight: 600,
-          }}
-        >
-          Recent Registered Companies
-        </h3>
-      ) : (
-        <h4
-          style={{
-            marginLeft: "4%",
-            marginTop: "15px",
-            marginBottom: "0px",
-            fontWeight: 600,
-            color: "#7A7A7A",
-          }}
-        >
-          Recent Registered Companies
-        </h4>
-      )}
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "-19px",
-        }}
-      >
-        <div
-          style={{
-            width: m1 ? "95%" : "100%",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <motion.div
-            style={{
-              cursor: "grab",
-              overflow: "hidden",
-              width: m1 ? "100%" : "100%",
-            }}
-            ref={caro}
-          >
-            <motion.div
-              drag="x"
-              dragConstraints={{
-                right: 0,
-                left: -width,
-              }}
-              style={{ display: "flex" }}
-              id="shiftblogs"
-            >
-              {fCompanies &&
-                fCompanies.map((item, index) => {
-                  return (
-                    <motion.div
-                      style={{
-                        minWidth: m1 ? "30%" : "50%",
-                        minHeight: m1 ? "250px" : "150px",
-                        marginLeft: index == 0 ? "1.5%" : "0px",
-                        marginRight:
-                          index === fCompanies.length - 1 ? "120px" : "5px",
-                        borderRadius: "10px",
-                        marginBottom: "20px",
-                      }}
-                    >
-                      <div
-                        key={item.id}
-                        style={{
-                          width: "100%",
-                        }}
-                      >
-                        <div style={{ width: "100%", height: "30px" }}></div>
-                        <Paper
-                          style={{
-                            width: "94%",
-                            display: "flex",
-                            justifyContent: "center",
-                            marginLeft: "3%",
-                            backgroundImage: `url(${item.logo})`,
-                            height: m1 ? "250px" : "170px",
-                            backgroundSize: "100% auto",
-                            backgroundPosition: "center",
-                            zIndex: 3,
-                            position: "relative",
-                            top: 0,
-                            left: 0,
-                            right: 0,
-                            bottom: 0,
-                            // boxShadow: `inset 0 0 0 50vw rgba(0,0,0,${0.5})`,
-                            boxShadow: `inset 0 -60px 50px 0px rgba(0, 0, 0, 0.5)`,
-                            borderRadius: "10px",
-                            transition: "transform .3s",
-                            backgroundRepeat: "no-repeat",
-                          }}
-                          onMouseOver={(e) => {
-                            e.target.style.transform = "scale(1.03)";
-                            e.target.style.cursor = "pointer";
-                          }}
-                          onMouseOut={(e) => {
-                            e.target.style.transform = "scale(1)";
-                          }}
-                          onClick={() => {
-                            // if (m1) return;
-                            // window.open(item.url);
-                            // alert("need the logic");
-                          }}
-                          elevation={5}
-                        ></Paper>
-                        <div
-                          style={{
-                            width: "100%",
-                            zIndex: 4,
-                            position: "relative",
-
-                            marginTop:
-                              item.name && item.name.length > 16
-                                ? "-70px"
-                                : "-70px",
-                            height: "50px",
-                            display: "flex",
-                            justifyContent: "center",
-                          }}
-                          onClick={() => {
-                            // window.open(item.url);
-                            alert("need the logic");
-                          }}
-                        >
-                          <h1
-                            style={{
-                              color: "white",
-                              textAlign: "left",
-                              fontWeight: 600,
-                              fontSize: m1 ? "25px" : "17px",
-                              textTransform: "capitalize",
-                              width: "80%",
-                              maxWidth: "80%",
-                              marginTop: "17px",
-                            }}
-                          >
-                            {item &&
-                              item.name.substr(0, m1 ? 26 : 14) +
-                                (item && item.name.length > (m1 ? 26 : 14)
-                                  ? "..."
-                                  : "")}
-                          </h1>
-                        </div>
-                        <div
-                          style={{
-                            width: "100%",
-                            zIndex: 4,
-                            position: "relative",
-                            marginTop: "-32px",
-                            height: "50px",
-                            display: "flex",
-                            justifyContent: "center",
-                          }}
-                          onClick={() => {
-                            // window.open(item.url);
-                            alert("need the logic");
-                          }}
-                        >
-                          <h6
-                            style={{
-                              color: "white",
-                              textAlign: "left",
-                              fontWeight: 100,
-                              textTransform: "capitalize",
-                              width: "80%",
-                              maxWidth: "80%",
-                            }}
-                          >
-                            Registered {times[index]} ago
-                          </h6>
-                        </div>
-                        <br />
-                      </div>
-                    </motion.div>
-                  );
-                })}
-            </motion.div>
-          </motion.div>
-        </div>
-      </div>
+      {fCompanies && fCompanies.length > 0 && times && times.length > 0 ? (
+        <Company3UI fCompanies={fCompanies} times={times} />
+      ) : null}
     </div>
   );
 }
